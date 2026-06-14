3e2922edfc55d0afc80635f4aaa554e30cccbfa4
RAJNEESH <23f3002888@ds.study.iitm.ac.in>
2026-06-14 22:21:50 +0530

    Update app (3).py

--- app (3).py
+++ app (3).py
@@ -29,7 +29,7 @@ div[data-testid="stMetricLabel"]   { color: #aaaaaa; }
 # ── LOAD DATA ─────────────────────────────────────────────────
 @st.cache_data
 def load_data():
-    return pd.read_csv("/content/drive/MyDrive/airline_project/master_final.csv")
+    return pd.read_csv("master_final.csv")
 
 df = load_data()
 
